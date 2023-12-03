2e3d65978615fe2b463efc8774326a380aaa027d
OfflineBot <FlintStoneLG@gmail.com>
2023-12-03 09:38:28 +0100

    single vec2

--- src/single_vec/single_vec.rs
+++ src/single_vec/single_vec.rs
@@ -11,7 +11,17 @@ impl<T> SingleVec<T>
 where
     T: for<'a> Deserialize<'a>
 {
-    pub fn from_vec1(path: &str) -> Vec<T> {
+    pub fn read_vec1(path: &str) -> Vec<T> {
+        let mut file = match File::open(path) {
+            Ok(data) => data,
+            Err(_) => panic!("Could not open File!")
+        };
+        let mut buffer = String::new();
+        file.read_to_string(&mut buffer).unwrap();
+        serde_json::from_str(&buffer).unwrap()
+    }
+
+    pub fn read_vec2(path: &str) -> Vec<Vec<T>> {
         let mut file = match File::open(path) {
             Ok(data) => data,
             Err(_) => panic!("Could not open File!")
